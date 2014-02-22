1cbb0bbaf294de41bbe335446cd47d23101caf70
Dylan Young <dylanyoungmeijer@gmail.com>
2014-02-21 22:18:07 -0400

    Runs on Server now --> node app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
